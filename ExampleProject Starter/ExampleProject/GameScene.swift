ba613077e67bb3e0239b83ea31e48a477cbb0179
sinewavemultimedia <sinewavemultimedia@gmail.com>
2017-05-04 00:45:21 -0700

    Intial Commit
    Initial Commit

---
+++ ExampleProject Starter/ExampleProject/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  ExampleProject
+//
+//  Created by James Tyner on 4/26/17.
+//  Copyright © 2017 James Tyner. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+
+    override func didMove(to view: SKView) {
+       
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+
+}
